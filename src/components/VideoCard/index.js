b8ecb5b0382ac62c71bf9fd1ebdfe04698780c32
Fhelipe Rodrigues <fhelipe.rodrigues@fatec.sp.gov.br>
2021-07-28 22:21:41 -0300

    Removendo imagens

--- src/components/VideoCard/index.js
+++ src/components/VideoCard/index.js
@@ -11,7 +11,6 @@ function VideoCard() {
         <Container>
             <ImageVideoContainer>
                 <Thumbnail>
-                    <Image className="thumbnail" alt="thumbnail" layout='fill' src="thumbnail.png" loader={()=> "https://images.unsplash.com/photo-1627442033472-a0eea0c96f98?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80"}/>
                 </Thumbnail>
             </ImageVideoContainer>
             <VideoChannelContainer>
